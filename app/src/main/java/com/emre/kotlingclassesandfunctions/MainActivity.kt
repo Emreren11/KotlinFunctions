904fec0cb62da8affe17fb114231dc77b9f5fa61
Emre <emreren.3637@gmail.com>
2023-05-05 12:06:49 +0300

    Second Commit

--- app/src/main/java/com/emre/kotlingclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/emre/kotlingclassesandfunctions/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.emre.kotlingclassesandfunctions
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myText: TextView
+    lateinit var button: Button
+    lateinit var nameText: EditText
+    lateinit var ageText: EditText
+    lateinit var jobText: EditText
+    var age: Int? = 0
+    var name = ""
+    var job = ""
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        myText = findViewById(R.id.myText)
+        button = findViewById(R.id.button)
+        nameText = findViewById(R.id.nameText)
+        ageText = findViewById(R.id.ageText)
+        jobText = findViewById(R.id.jobText)
+
+
+        /*
         sum(5, 6)
         println(multiply(5, 7)) // return sonucunu parantez içine verir
+        */
+
 
         //Classes
         val homer = Simpson("Homer", 50, "Nuclear")
@@ -65,5 +91,22 @@ class MainActivity : AppCompatActivity() {
 
     fun multiply(x: Int, y: Int): Int {
         return x * y
+    }
+
+    fun buttonClicked(view: View) {
+        name = nameText.text.toString()
+        age = ageText.text.toString().toIntOrNull() // değeri nullable yapar (Int?)
+        job = jobText.text.toString()
+
+        if (age != null) {
+
+            val simpson = Simpson(name, age!!, job)
+
+            myText.text = "Name: ${simpson.name} - Age: ${simpson.age} - Job: ${simpson.job}"
+        } else {
+            myText.text = "Enter age"
+        }
+
+
     }
 }
