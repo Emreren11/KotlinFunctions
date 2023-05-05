00c3d5ac69ff16e98806937cff1d4df4ea191719
Emre <emreren.3637@gmail.com>
2023-05-05 11:38:13 +0300

    Second Commit

--- app/src/main/java/com/emre/kotlingclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/emre/kotlingclassesandfunctions/MainActivity.kt
@@ -18,17 +18,52 @@ class MainActivity : AppCompatActivity() {
         homer.age = 50
         homer.job = "Nuclear"*/
         println(homer.name)
+
+
+        //Nullability
+
+        var myString: String? = null // "?" değerin null olma ihtimalinden dolayı konur
+        println(myString) // Çıktısını "null" olur
+
+        var myAge: Int? = null
+        myAge = 20
+
+        // !! -> myAge kesin boş değil
+        //println(myAge!! * 50)
+
+        // ? -> myAge boş olabilir
+        //println(myAge!! * 50)
+
+        //elvis operator
+
+        println(myAge?.minus(10) ?: -10) // myAge null ise -10 yazdırır.
+
+        //if - else
+
+        if (myAge != null) {
+            println(myAge.minus(10))
+        } else {
+            println("myAge is null")
+        }
+
+        // if - else ile yerine
+
+        myAge?.let { // it -> eğer myAge null değilse myAge değerini alır
+            println(it * 10)
+            //myAge null ise bu alan çalışmaz
+        }
+
     }
 
     fun test() { // Fonksiyon oluşturma
 
     }
 
-    fun sum(a : Int, b : Int) { // Unit fonksiyon -> geri dönüt vermeyen foksiyonlar (void)
+    fun sum(a: Int, b: Int) { // Unit fonksiyon -> geri dönüt vermeyen foksiyonlar (void)
         println(a + b)
     }
 
-    fun multiply(x : Int, y : Int) : Int {
+    fun multiply(x: Int, y: Int): Int {
         return x * y
     }
 }
